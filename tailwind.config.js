ae77b7c32286eaa8362b023163af0325ce02203c
easyone1372 <easyone1372@office.duksung.ac.kr>
2024-07-08 14:18:51 +0900

    edit

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      width: {
+        width_1200: "1200px",
+      },
+    },
   },
   plugins: [],
 };
